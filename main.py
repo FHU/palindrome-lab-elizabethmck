f0f1fd3f042132133caeebc1ae564be293dab81c
elizabethmck <elizabeth.mckeeby@students.fhu.edu>
2024-02-09 10:38:43 -0600

    Update main.py
    moved function.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 def palindrome(word):
     word = word.lower()
-    word = word.replace("","")
+    word = word.replace(" ","")
     if word == word[::-1]:
         return True
     else:
         return False
 
-print(palindrome(word))
-
 if __name__ == '__main__':
     word = input()
+    print(palindrome(word))
